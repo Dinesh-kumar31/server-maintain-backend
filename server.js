7ef004879e76bb0e1a326f59983908e6e8c8764c
Hari <hariharan.gaipp@gmail.com>
2022-03-05 11:02:57 +0530

    db connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -8,7 +9,17 @@ const PORT = 8080;
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+mongoose.connect('mongodb://localhost:27017/local',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully to mongodb");
+});
 app.listen(PORT, () => {
     console.log(`Server us running on ${PORT}`)
 })
